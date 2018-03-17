c48429955ca6a8929fbd713d14904daaa3373a8b
Entech student <student@entech281.org>
2018-03-17 14:21:06 -0400

    Change the buttons so all are simple buttons

--- Arduino/sketch_Macarana/sketch_Macarana.ino
+++ Arduino/sketch_Macarana/sketch_Macarana.ino
@@ -6,38 +6,24 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
         /* includeRxRyRzAxes */ false, false, false,
         /* includeThrottle, Accelerator, Brake, Steering */ false, false, false, false);
 
+#define NUM_BUTTONS 13
+int lastButtonState[NUM_BUTTONS];
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-    
+  for (int i=0; i<NUM_BUTTONS; i++) {
+      pinMode(i, INPUT_PULLUP);
+      lastButtonState[i] = 0;
+  }    
   Joystick.begin();
 }
 
-int lastButtonState[13] = {0,0,0,0,0,0,0,0,0,0,0,0};
 
 void loop() {
   // put your main code here, to run repeatedly:
   for (int index = 0; index < 13; index++)
   {
     int currentButtonState = !digitalRead(index);
-    if ((index == 12) || (lastButtonState[index-1])) {
-      currentButtonState = !currentButtonState;
-    }
-    if ((index == 10) || (lastButtonState[index-1])) {
-      currentButtonState = !currentButtonState;
-    }
     if (currentButtonState != lastButtonState[index])
     {
       Joystick.setButton(index, currentButtonState);
